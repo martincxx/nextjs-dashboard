ad950b6376005d703c87e3ec46e231a5745fe444
Martin Coronel <martincxx@gmail.com>
2024-03-12 20:40:30 +0200

    Add revenue chart with db data

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,15 @@
+import Link from 'next/link';
 export default function Page() {
-  return <p>Login</p>;
+  return (
+    <>
+      <p>Login</p>
+      <Link
+        href="/"
+        key="back"
+        className="flex max-w-fit items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+      >
+        Back
+      </Link>
+    </>
+  );
 }
