e1b851058c2d0d0ac2155f92aa0d84f3fbea9b1e
Martin Coronel <martincxx@gmail.com>
2024-03-29 15:20:34 +0200

    Add Authentication config and login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,17 @@
-import Link from 'next/link';
-export default function Page() {
+import AcmeLogo from '@/app/ui/acme-logo';
+import LoginForm from '@/app/ui/login-form';
+
+export default function LoginPage() {
   return (
-    <>
-      <p>Login</p>
-      <Link
-        href="/"
-        key="back"
-        className="flex max-w-fit items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-      >
-        Back
-      </Link>
-    </>
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            <AcmeLogo />
+          </div>
+        </div>
+        <LoginForm />
+      </div>
+    </main>
   );
 }
